9014df4cd6bceb9c4046ce43c514c7103d2f1102
Daniela Baron <baron.daniela@gmail.com>
2013-07-20 16:59:01 -0400

    rest service for user

---
+++ SpringLoginAngular/ArchApp/src/main/java/ca/danib/sample/UserController.java
@@ -0,0 +1,37 @@
+package ca.danib.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	 public @ResponseBody UserTransfer getUser() {
+		Map<String, Boolean> roles = new HashMap<String, Boolean>();
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Object principal = authentication.getPrincipal();
+		if (principal instanceof String && ((String) principal).equals("anonymousUser")) {
+			return new UserTransfer("anonymous", roles);
+		}
+		UserDetails userDetails = (UserDetails) principal;
+
+		for (GrantedAuthority authority : userDetails.getAuthorities()) {
+			roles.put(authority.toString(), Boolean.TRUE);
+		}
+
+		return new UserTransfer(userDetails.getUsername(), roles);
+	 }
+
+}
